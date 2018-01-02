5ae36ddfd1c9d35274ca4ef479d3f7dbee34e7cb
aartipathania <aarti.pathania@gmail.com>
2018-01-02 13:33:56 -0800

    Commiting again the HomeDepot module with homeDepotTestRunner.xml file changes

--- HomeDepot/src/main/java/page_objects/SearchPage.java
+++ HomeDepot/src/main/java/page_objects/SearchPage.java
@@ -1,4 +1,29 @@
 package page_objects;
 
-public class SearchPage {
+import base.CommonAPI;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SearchPage extends CommonAPI {
+
+    @FindBy(id="headerSearch")
+     public static WebElement searchbox;
+
+    @FindBy(xpath = "//*[@id=\'products\']/div/div[1]/div/div[10]/div/a/span")
+    public static WebElement addToCart;
+
+    public static void searchProduct() throws InterruptedException {
+        searchbox.sendKeys("i phone", Keys.ENTER);
+        System.out.println("Current Title: "+driver.getTitle());
+
+        //Click add to cart and continue with the next page
+        addToCart.click();
+
+        System.out.println("Page title of search phone: "+driver.getTitle());
+        Thread.sleep(2000);
+    }
+
+
+
 }
--- HomeDepot/src/test/java/testCases/TestSearchPage.java
+++ HomeDepot/src/test/java/testCases/TestSearchPage.java
@@ -1,4 +1,14 @@
 package testCases;
 
-public class SearchPageTest {
+import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import page_objects.SearchPage;
+
+public class TestSearchPage extends SearchPage {
+    @Test
+    public void testSearchProduct() throws InterruptedException {
+        SearchPage sr = PageFactory.initElements(driver, SearchPage.class);
+        sr.searchProduct();
+    }
 }
