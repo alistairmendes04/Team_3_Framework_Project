24c1e6898998cc156a9fcbcdac1e3e779ea8b084
jahidul2543 <jahidul2543@gmail.com>
2018-01-03 09:22:33 -0500

    changes to test file

--- AliBaba/src/test/java/testCases/TestSearchOption.java
+++ AliBaba/src/test/java/testCases/TestSearchOption.java
@@ -15,18 +15,18 @@ public class TestSearchOption extends CommonAPI {
 
     @Test (priority = 1)
     public void testSupplierSearch(){
-        HomePage sr1 = PageFactory.initElements(driver, HomePage.class);
-        sr1.searchSuppliers();
+        HomePage sr = PageFactory.initElements(driver, HomePage.class);
+        sr.searchSuppliers();
     }
     @Test (priority = 3)
     public void  testQuotesSearch(){
-        HomePage sr2 = PageFactory.initElements(driver, HomePage.class);
-        sr2.quotesSearchOption();
+        HomePage sr = PageFactory.initElements(driver, HomePage.class);
+        sr.quotesSearchOption();
     }
     @Test
     public void sourcingSolutions(){
-        HomePage sr3 = PageFactory.initElements(driver, HomePage.class);
-        sr3.sourcingSolutions();
+        HomePage sr = PageFactory.initElements(driver, HomePage.class);
+        sr.sourcingSolutions();
     }
 
 }
