decd674ecd5b0fca1e0d66740c860c8c8934c61d
jahidul2543 <jahidul2543@gmail.com>
2018-01-04 17:37:03 -0500

    Updated Common API

--- AliBaba/src/main/java/page_objects/HomePage.java
+++ AliBaba/src/main/java/page_objects/HomePage.java
@@ -1,7 +1,6 @@
 package page_objects;
 
 import base.CommonAPI;
-
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -13,13 +12,13 @@ public class HomePage extends CommonAPI {
     @FindBy(name = "SearchText")
     public static WebElement searchBox;
 
-    @FindBy (xpath = "//div[@id=\"J_SC_header\"]//form/div[1]/div/div/span[1]")
+    @FindBy(xpath = "//div[@id=\"J_SC_header\"]//form/div[1]/div/div/span[1]")
     public static WebElement productSearchOption;
 
     @FindBy(xpath = "//div[@id=\"J_SC_header\"]//form/div[1]/div/div/span[1]")
     public static WebElement suppliersSearchOption;
 
-    @FindBy (xpath = "//div[@id=\"J_SC_header\"]//form/div[1]/div/div/span[2]")
+    @FindBy(xpath = "//div[@id=\"J_SC_header\"]//form/div[1]/div/div/span[2]")
     public static WebElement quotesSearchOptin;
 
     @FindBy(xpath = "//*[@id=\"J_SC_header\"]//input[4]")
@@ -31,9 +30,33 @@ public class HomePage extends CommonAPI {
     @FindBy(partialLinkText = "Top Selected")
     public static WebElement topSelectedSuppliers;
 
+    @FindBy(partialLinkText = "Sign")
+    public static WebElement signInButton;
 
+    @FindBy(partialLinkText = "Get the App")
+    public static WebElement getTheAppLink;
+
+    @FindBy(partialLinkText = "One Request")
+    public static WebElement oneRequestButton;
+
+    @FindBy(partialLinkText = "Favorites")
+    public static WebElement favoriteLink;
+
+    @FindBy(xpath = "//div[@id=\"J_SC_header\"]/header/div[2]/div[1]/div/div/h3")
+    public static WebElement categories;
+
+    @FindBy(partialLinkText = "Machinery")
+    private WebElement machinery;
+
+    @FindBy(partialLinkText = "Hand Tools")
+    private WebElement handTools;
+
+    @FindBy(xpath = "//a[@class='more' and @href='javascript:;']")
+    private WebElement seeMore;
 
     public void searchProduct() {
+        String url = driver.getCurrentUrl();
+        org.testng.Assert.assertEquals(url, "https://www.alibaba.com/");
         // search by product search option
         searchBox.sendKeys("I phone", Keys.ENTER);
         System.out.println("Get Product Search Page Title: " + driver.getTitle());
@@ -47,7 +70,8 @@ public class HomePage extends CommonAPI {
         System.out.println("Suppliers Search Page Titel: " + driver.getTitle());
 
     }
-    public void  quotesSearchOption(){
+
+    public void quotesSearchOption() {
         productSearchOption.click();
         quotesSearchOptin.click();
         searchBox.sendKeys("laptop bag");
@@ -55,14 +79,37 @@ public class HomePage extends CommonAPI {
     }
 
     public void sourcingSolutions() {
-        //click Sourcing Solutions
-        //sourcingSolutions.click();
-
+        //hover Sourcing Solutions
         Actions act = new Actions(driver);
         act.moveToElement(sourcingSolutions).build().perform();
+        // clcik top selected suppliers
+        boolean bl = topSelectedSuppliers.isEnabled();
+       // boolean bl = isEnableStatus(driver,topSelectedSuppliers);
+        System.out.println("Top Selected Suppliers link status: " + bl);
+        System.out.println(topSelectedSuppliers.getText());
         topSelectedSuppliers.click();
         System.out.println("Page titel for Sourcing Solutions: " + driver.getTitle());
     }
 
+    public void clikSignIn() {
+        signInButton.click();
+        //continue in Sign In Page
+    }
+
+    public void homePageButtonStatus() {
+        System.out.println("Get The App link status: " + getTheAppLink.isEnabled());
+        System.out.println("'One Request' link status: " + oneRequestButton.isEnabled());
+        System.out.println("favorite link status: " + favoriteLink.isEnabled());
+
+    }
+
+    public void categories() {
+        //hover over Categories
+        Actions act = new Actions(driver);
+        act.moveToElement(categories).moveToElement(machinery).build().perform();
+        handTools.click();
+
+
+    }
 
 }
---
+++ AliBaba/src/main/java/page_objects/ProductsPage.java
@@ -0,0 +1,61 @@
+package page_objects;
+
+import base.CommonAPI;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductsPage extends CommonAPI {
+    @FindBy(xpath = "//span[contains(text(),'Gold Supplier')][1]")
+    private WebElement goldSuppliers;
+
+    @FindBy(xpath = "//a[@class='more' and @href='javascript:;']")
+    private WebElement seeMore;
+
+   /* @FindBy (partialLinkText = "Wrench ")
+    private WebElement wrench;*/
+
+    @FindBy(partialLinkText = "Leather crafting Tools ")
+    private WebElement leatherCraftingTools;
+
+    @FindBy (xpath = "//span[text()='Free samples']")
+    private WebElement freeSamples;
+
+    @FindBy (xpath = "//span[(text()='Asia ')][1]")
+    private WebElement asia;
+
+    @FindBy (xpath = "//input[@id=\"J-domdot-refine-moqt\"]")
+    private WebElement minOrder;
+
+    @FindBy (xpath = "//div[1]/div/div[2]/div[3]/a[1]")
+    private WebElement contactSupplier;
+
+    @FindBy (xpath = "//*[@id=\"tinymce\"]")
+    private WebElement messageBox;
+
+    @FindBy (name = "email")
+    private WebElement email;
+
+    @FindBy (xpath = "//*[@id=\"inquiry-body\"]//div[5]/div[4]/div/input")
+    private  WebElement inquiryBody;
+
+    public void toolsSearch() throws InterruptedException {
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+        }
+        System.out.println("Hand Tools Page Title: " + driver.getTitle());
+        seeMore.click();
+        leatherCraftingTools.click();
+        freeSamples.click();
+      /* minOrder.sendKeys("10", Keys.ENTER);
+        contactSupplier.click();
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+           *//* messageBox.sendKeys("Red 4pcs, Blue 6 pcs ");
+            email.sendKeys("testdata@gmail.cm");
+            inquiryBody.click();*//*
+
+        }*/
+
+    }
+}
